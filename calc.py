d99b93b0eb78b188255e136a028afd53c23fefec
Gregorio Robles <grex@computer.org>
2015-09-24 22:52:32 +0200

    Ficheros de inicio de práctica y de comprobación de entrega correcta

---
+++ calc.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+def plus(op1, op2):
+    """ Function to sum the operands """
+    return op1 + op2
+
+
+def minus(op1, op2):
+    """ Function to substract the operands """
+    return op1 - op2
+
+if __name__ == "__main__":
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+
+    if sys.argv[2] == "suma":
+        result = plus(operando1, operando2)
+    elif sys.argv[2] == "resta":
+        result = minus(operando1, operando2)
+    else:
+        sys.exit('Operación sólo puede ser sumar o restar.')
+
+    print result
