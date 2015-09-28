d2bcc71e0afc889bbf0ee8f21f4e90755905e7c4
cguirado <cgboluda@gmail.com>
2015-09-28 16:29:39 +0200

    Añadido - * Terminado Ap4

--- calcoo.py
+++ calcoo.py
@@ -16,13 +16,15 @@ if __name__ == "__main__":
         operando1 = int(sys.argv[1])
         operando2 = int(sys.argv[3])
         operacion = sys.argv[2]
-    
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+        
     calcu = Calculadora()
     if operacion == "suma":
         resultado = calcu.suma(operando1,operando2)
     elif operacion == "resta":
         resultado = calcu.resta(operando1,operando2)
-    else
+    else:
         print("Operaciones validas SUMAS O RESTAS")
 
     print (resultado)
