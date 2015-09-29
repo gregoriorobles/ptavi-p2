d74cb056a4a05726c657295bcb26024c3dc4389d
muris2016 <rsemuris2016@gmail.com>
2015-09-29 17:31:41 +0200

    Clase Calculadora mejorada

--- calcoo.py
+++ calcoo.py
@@ -2,9 +2,16 @@
 # -*- coding:utf-8 -*-
 
 import sys
+import calc
 
 
-class Calculadora():
+class Calculadora:
+    def __init__(self):
+        self.operations = {"suma": self.sum, "resta": self.substraction}
+
+    def operating(self, calculator_operation, op1, op2):
+        return calculator_operation(op1, op2)
+
     def sum(self, a, b):
         return a + b
 
@@ -12,33 +19,18 @@ class Calculadora():
         return a - b
 
 
-def to_number(num):
+
+def do_operation(calculator, operation, op1, op2):
+
     try:
-        if '.' in num:
-            return float(num)
-        else:
-            return int(num)
-    except:
-        sys.exit("Error")
-
-
-def do_operation(operation, op1, op2):
-    calculator = Calculadora()
-
-
-    if operation == "suma":
-        result = calculator.sum(op1, op2)
-        print(result)
-
-    elif operation == "resta":
-        result = calculator.substraction(op1, op2)
-        print(result)
-
-    else:
-        print ("Not allowed operation", operation)
+        return calculator.operating(calculator.operations[operation],
+                                    op1, op2)
+    except KeyError:
+        sys.exit("Not allowed operation " + operation)
 
 if __name__ == "__main__":
-    op1 = to_number(sys.argv[1])
+    op1 = calc.to_number(sys.argv[1])
     operation = sys.argv[2]
-    op2 = to_number(sys.argv[3])
-    do_operation(operation, op1, op2)
+    op2 = calc.to_number(sys.argv[3])
+    calculator = Calculadora()
+    print(do_operation(calculator, operation, op1, op2))
