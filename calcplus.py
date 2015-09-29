6dfb48f5fd82a68373a3f7aae21e1705870596cc
muris2016 <rsemuris2016@gmail.com>
2015-09-29 18:13:09 +0200

    Nombres de variables mejorados

--- calcplus.py
+++ calcplus.py
@@ -6,19 +6,19 @@ import calcoohija
 import calcoo
 
 
-def operating(calculator, operation, numbers):
+def operating(calc, operation, numbers):
     ans = int(numbers[0])
     for num in numbers[1:]:
-        ans = calcoo.do_operation(calculator, operation, ans, int(num))
+        ans = calcoo.do_operation(calc, operation, ans, int(num))
     print(ans)
 
 
 if __name__ == "__main__":
     script, filename = sys.argv
-    calculator = calcoohija.CalculadoraHija()
+    calc = calcoohija.CalculadoraHija()
     file = open(filename)
     for line in file.readlines():
         operation = line.split(',')[0]
         numbers = line.split(',')[1:]
-        operating(calculator, operation, numbers)
+        operating(calc, operation, numbers)
     file.close
