7817eb60f94a3a3ea6181e0ed883a0554d64d4f0
c.guirado <cgboluda@gmail.com>
2015-09-30 17:50:04 +0200

    Preguntar

--- calcplusplus.py
+++ calcplusplus.py
@@ -2,13 +2,45 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import calcplus
 import csv
-fichero= sys.argv[1]
-with open(fichero) as csvarchivo:
-    lineas = csv.reader(csvarchivo)
-    for linea in lineas:
-        print(linea)
-       
-        operacion, op1, op2 = linea
-        print(operacion, op1, op2, op3)
+import calcoohija
+if __name__ == "__main__":
+    calcu= calcoohija.CalculadoraHija()
+    try:
+        with open('fichero.csv') as csvarchivo:
+            lineas = csv.reader(csvarchivo)
+            for linea in lineas:
+                print(linea)
+                operador= linea[0]
+                print(operador)
+                if operador == "suma":
+                        sum1=0
+                        for operaciones in linea[1:]:
+                            sum1=calcu.suma (sum1, float(operaciones))
+                        print(sum1)
+                        
+                if operador == "resta":
+                    rest1= calcu.resta(float(linea[1]), float(linea[2]))
+                    for operaciones in linea[3:]:
+                        rest1=calcu.resta(rest1, float(operaciones))
+                    print(rest1)
+                
+                if operador == "multiplica":
+                    mult=1
+                    for operaciones in linea[1:]:
+                        mult=calcu.multiplica(mult, float(operaciones))
+                    print(mult)
+                
+                if operador == "divide":
+                    if float(linea[2])==0:
+                        sys.exit("Division by zero is not allowed")
+                    else:
+                        div=calcu.divide(float(linea[1]),float(linea[2]))
+                    for operaciones in linea[3:]:
+                        if float(operaciones)==0:
+                            sys.exit("Division by zero is not allowed")
+                        else:
+                            div=calcu.divide(div, float(operaciones))
+                    print(div)
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
