fb685fc4a9d599bc15d4eda9ab8c459a75eba622
robertoyubero <robertoyubero@gmail.com>
2015-10-02 17:45:33 +0200

    pasado pep8

--- calcoohija.py
+++ calcoohija.py
@@ -5,28 +5,41 @@
 
 import sys
 
+
 class Calculadora:
 
     def suma(self, op1, op2):
         """ Funcion suma"""
         return op1 + op2
 
-
     def resta(self, op1, op2):
         """ Funcion resta """
         return op1 - op2
-    
-    
+
+
 class CalculadoraHija(Calculadora):
 
     def multiplicacion(self, operando1, operando2):
         return operando1 * operando2
-        
+
     def division(self, operando1, operando2):
         if operando2 == 0:
             sys.exit("Division by zero is not allowed")
         else:
             return operando1 / operando2
+
+    def operar(self, operacion, op1, op2):
+
+        if operacion == "suma":
+            return self.suma(op1, op2)
+        elif operacion == "resta":
+            return self.resta(op1, op2)
+        elif operacion == "multiplica":
+            return self.multiplicacion(op1, op2)
+        elif operacion == "divide":
+            return self.division(op1, op2)
+        else:
+            sys.exit('Operaciones admitidas: suma, resta, multiplicacion y division')
 
 
 if __name__ == "__main__":
@@ -37,21 +50,8 @@ if __name__ == "__main__":
         operando1 = int(sys.argv[1])
         operando2 = int(sys.argv[3])
     except ValueError:
-        sys.exit("Error: solo operandos numericos!")
-        
-    #dependiendo de los argumentos realizamos una operacion u otra       
-    if sys.argv[2] == "suma":
-        resultado = miCalculadora.suma(operando1, operando2)
-    elif sys.argv[2] == "resta":
-        resultado = miCalculadora.resta(operando1, operando2)
-    elif sys.argv[2] == "multiplica":
-        resultado = miCalculadora.multiplicacion(operando1, operando2)
-    elif sys.argv[2] == "divide":
-        resultado = miCalculadora.division(operando1, operando2)    
-    else:
-        sys.exit('Solo operaciones de suma, resta, multiplicacion y division')
+        sys.exit("Error: solo operandos numericos enteros")
 
-    print(resultado)        
-        
-        
-  
+    #realizamos la operacion
+    resultado = miCalculadora.operar(sys.argv[2], operando1, operando2)
+    print(resultado)
