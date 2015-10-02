ebc8918217e3695a1930eaba515d87748a9bb5d5
robertoyubero <robertoyubero@gmail.com>
2015-10-02 17:45:45 +0200

    pasado pep8

--- calcplusplus.py
+++ calcplusplus.py
@@ -10,14 +10,15 @@ import sys
 with open(sys.argv[1], 'rd') as f:
     lista_lineas = csv.reader(f)
     miCalculadora = calcoohija.CalculadoraHija()
-    
+
     for linea in lista_lineas:
-        operacion = linea[0]       
-        resultado = int(linea[1])                       #el primer operando lo meto directamente en resultado 
-              
-        for k in range(2,len(linea)):                   #recorro la linea desde 2operando
+        operacion = linea[0]
+        #el primer operando lo meto directamente en resultado
+        resultado = int(linea[1])
+
+        #recorro la linea desde 2operando
+        for k in range(2, len(linea)):
             operando = int(linea[k])
             resultado = miCalculadora.operar(operacion, resultado, operando)
-            
+
         print(operacion + " = " + str(resultado))
-      
