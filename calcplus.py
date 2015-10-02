41043ea0260d49947fdebed097a0db29def3ef46
robertoyubero <robertoyubero@gmail.com>
2015-10-02 17:45:40 +0200

    pasado pep8

--- calcplus.py
+++ calcplus.py
@@ -7,29 +7,30 @@ import sys
 import calcoohija
 
 
-
 if __name__ == "__main__":
 
     fichero = open(sys.argv[1], 'r')
-    lista_lineas = fichero.read()     #leo todo el fichero   
-    lista_lineas = lista_lineas.splitlines() #creamos una lista de lineas sin salto de linea
+    #leo todo el fichero
+    lista_lineas = fichero.read()
+    #creamos una lista de lineas sin salto de linea
+    lista_lineas = lista_lineas.splitlines()
     miCalculadora = calcoohija.CalculadoraHija()
-    
-    
-    for i in range(0,len(lista_lineas)):                      #recorro la lista linea por linea   
-        lista_lineas[i] = lista_lineas[i].split(',')    #eliminamos las comas de cada linea
-        linea = lista_lineas[i]                         #separo cada linea y la guardo en una variable                       
-        operacion = linea[0]                            #elijo el tipo de operacion de la linea      
-        resultado = int(linea[1])                       #el primer operando lo meto directamente en resultado
-        
-        for k in range(2,len(linea)):                   #recorro la linea desde 2operando
+
+    #recorro la lista linea por linea
+    for i in range(0, len(lista_lineas)):
+        #eliminamos las comas de cada linea
+        lista_lineas[i] = lista_lineas[i].split(',')
+        #separo cada linea y la guardo en una variable
+        linea = lista_lineas[i]
+        #elijo el tipo de operacion de la linea
+        operacion = linea[0]
+        #el primer operando lo meto directamente en resultado
+
+        #recorro la linea desde 2operando
+        for k in range(2, len(linea)):
             operando = int(linea[k])
             resultado = miCalculadora.operar(operacion, resultado, operando)
-            
-        print(operacion + " = " + str(resultado))
-            
 
-    
-    
+        print(operacion + " = " + str(resultado))
 
     fichero.close()
