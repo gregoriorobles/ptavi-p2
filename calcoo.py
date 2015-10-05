0f91fca48f9a010d8b88933f45011b44e17b107c
muris2016 <rsemuris2016@gmail.com>
2015-10-05 15:40:02 +0200

    Modificado con respecto a calc.py

--- calcoo.py
+++ calcoo.py
@@ -20,15 +20,12 @@ class Calculadora:
 
 
 def do_operation(calc, operation, op1, op2):
-
     try:
         return calc.operating(calc.operations[operation], op1, op2)
     except KeyError:
         sys.exit("Not allowed operation " + operation)
 
 if __name__ == "__main__":
-    op1 = calc.to_number(sys.argv[1])
-    operation = sys.argv[2]
-    op2 = calc.to_number(sys.argv[3])
+    op1, operation, op2 = calc.take_args()
     calc = Calculadora()
     print(do_operation(calc, operation, op1, op2))
