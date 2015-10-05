b781dc7811968139c8f8309c802de7388693a425
muris2016 <rsemuris2016@gmail.com>
2015-10-05 15:41:08 +0200

    Errores sobre argumentos capturados

--- calcplus.py
+++ calcplus.py
@@ -2,19 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import calcoohija
+from calc import to_number
 import calcoo
+import calcoohija
+
+def capture_argv_error():
+    if len(sys.argv) != 2:
+        sys.exit("Error: incorrect parameters\nUse: script filename ")
 
 
 def operating(calc, operation, numbers):
     ans = int(numbers[0])
     for num in numbers[1:]:
-        ans = calcoo.do_operation(calc, operation, ans, int(num))
+        ans = calcoo.do_operation(calc, operation, ans, to_number(num))
     print(ans)
 
 
 if __name__ == "__main__":
-    script, filename = sys.argv
+    capture_argv_error()
+    filename = sys.argv[0]
     calc = calcoohija.CalculadoraHija()
     file = open(filename)
     for line in file.readlines():
