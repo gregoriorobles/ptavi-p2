303fa17de7a28f6e401e38d8ca76494a8b58fdf3
c.guirado <cgboluda@gmail.com>
2015-10-05 16:56:18 +0200

    comprimido

--- calcoo.py
+++ calcoo.py
@@ -5,19 +5,27 @@ import sys
 
 
 class Calculadora():
+
+    def check(self, op1, op2):
+        try:
+            op1 = float(op1)
+            op2 = float(op2)
+        except ValueError:
+            sys.exit("Error: Non numerical parameters")
+        return (op1, op2)
+
     def suma(self, op1, op2):
+        op1, op2 = self.check(op1, op2)
         return op1 + op2
 
     def resta(self, op1, op2):
+        op1, op2 = self.check(op1, op2)
         return op1 - op2
 
 if __name__ == "__main__":
-    try:
-        operando1 = float(sys.argv[1])
-        operando2 = float(sys.argv[3])
-        operacion = sys.argv[2]
-    except ValueError:
-        sys.exit("Error: Non numerical parameters")
+    operando1 = sys.argv[1]
+    operando2 = sys.argv[3]
+    operacion = sys.argv[2]
 
     calcu = Calculadora()
     if operacion == "suma":
