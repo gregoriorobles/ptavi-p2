6c9a05d25c2c14ce6877a63fcf1ba0a31593d523
c.guirado <cgboluda@gmail.com>
2015-10-05 11:41:02 +0200

    Pasado pep8

--- calcplus.py
+++ calcplus.py
@@ -6,42 +6,38 @@ import calcoohija
 
 if __name__ == "__main__":
 
-    leer = open(sys.argv[1],"r")
+    leer = open(sys.argv[1], "r")
     lineas = leer.readlines()
-    calcu= calcoohija.CalculadoraHija()
+    calcu = calcoohija.CalculadoraHija()
     try:
         for linea in (lineas):
             palabra = linea.split(",")
-            operador=palabra[0]
-            print(operador)
+            operador = palabra[0]
             if operador == "suma":
-                sum1=0
+                sum1 = 0
                 for operaciones in palabra[1:]:
-                    sum1=calcu.suma (sum1, float(operaciones))
+                    sum1 = calcu.suma(sum1, float(operaciones))
                 print(sum1)
-                
             if operador == "resta":
-                rest1= calcu.resta(float(palabra[1]), float(palabra[2]))
+                rest1 = calcu.resta(float(palabra[1]), float(palabra[2]))
                 for operaciones in palabra[3:]:
-                    rest1=calcu.resta(rest1, float(operaciones))
+                    rest1 = calcu.resta(rest1, float(operaciones))
                 print(rest1)
-            
             if operador == "multiplica":
-                mult=1
+                mult = 1
                 for operaciones in palabra[1:]:
-                    mult=calcu.multiplica(mult, float(operaciones))
+                    mult = calcu.multiplica(mult, float(operaciones))
                 print(mult)
-            
             if operador == "divide":
-                if float(palabra[2])==0:
+                if float(palabra[2]) == 0:
                     sys.exit("Division by zero is not allowed")
                 else:
-                    div=calcu.divide(float(palabra[1]),float(palabra[2]))
+                    div = calcu.divide(float(palabra[1]), float(palabra[2]))
                 for operaciones in palabra[3:]:
-                    if float(operaciones)==0:
+                    if float(operaciones) == 0:
                         sys.exit("Division by zero is not allowed")
                     else:
-                        div=calcu.divide(div, float(operaciones))
+                        div = calcu.divide(div, float(operaciones))
                 print(div)
     except ValueError:
         sys.exit("Error: Non numerical parameters")
