a71c4cca38101fea8d7aa28c695be102257704ff
c.guirado <cgboluda@gmail.com>
2015-10-05 11:40:43 +0200

    Pasado pep8

--- calcoohija.py
+++ calcoohija.py
@@ -4,11 +4,13 @@
 import sys
 import calcoo
 
+
 class CalculadoraHija (calcoo.Calculadora):
     def multiplica(self, op1, op2):
         return op1 * op2
-    def divide (self, op1, op2):
-        return op1 / op2 
+
+    def divide(self, op1, op2):
+        return op1 / op2
 
 if __name__ == "__main__":
     try:
@@ -17,25 +19,21 @@ if __name__ == "__main__":
         operacion = sys.argv[2]
     except ValueError:
         sys.exit("Error: Non numerical parameters")
-    
+
     calcu = CalculadoraHija()
 
     if operacion == "suma":
-        resultado = calcu.suma (operando1, operando2)
+        resultado = calcu.suma(operando1, operando2)
     elif operacion == "resta":
-        resultado = calcu.resta (operando1, operando2)
+        resultado = calcu.resta(operando1, operando2)
     elif operacion == "multiplica":
-        resultado = calcu.multiplica (operando1, operando2)
+        resultado = calcu.multiplica(operando1, operando2)
     elif operacion == "divide":
         if operando2 == 0:
             sys.exit("Division by zero is not allowed")
         else:
-            resultado = calcu.divide (operando1, operando2)
+            resultado = calcu.divide(operando1, operando2)
     else:
         print("Operacion no valida")
 
     print(resultado)
-
-
-            
-    
