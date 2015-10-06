6983fab551156130bab0ab6976fcd2176c7e33e5
Gregorio Robles <grex@gsyc.urjc.es>
2015-10-07 00:17:50 +0200

    Error en el nombre de la variable y en la secuencia

--- calcplusplus.py
+++ calcplusplus.py
@@ -9,14 +9,14 @@ if __name__ == "__main__":
 
     calcu = calcoohija.CalculadoraHija()
     fichero = sys.argv[1]
+    diccionario = {"suma": calcu.suma, "multiplica": calcu.multiplica, "resta": calcu.resta, "divide": calcu.divide} 
+
     with open(fichero) as csvarchivo:
         lineas = csv.reader(csvarchivo)
         for linea in lineas:
             operador = linea[0]
-            diccionario = {"suma": calcu.suma, "multiplica": calcu.multiplica, "resta": calcu.resta, "divide": calcu.divide} 
-
-        if operador in ["suma", "multiplica", "resta","divide"]:
-            calcular = palabra[1]
-            for operaciones in palabra[2:]:
-                calcular = diccionario[operador](calcular, operaciones)
-            print(calcular)
+            if operador in ["suma", "multiplica", "resta","divide"]:
+                calcular = linea[1]
+                for operaciones in linea[2:]:
+                    calcular = diccionario[operador](calcular, operaciones)
+                print(calcular)
